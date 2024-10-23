1f22eaadff19f257a0dbc487b0593a1ab63e03a2
Philip Johnson <johnson@hawaii.edu>
2024-10-22 16:08:35 -1000

    Update app_test.dart

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_auth_mocks/firebase_auth_mocks.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_mock_firebase_auth/app.dart';
 import 'package:flutter_mock_firebase_auth/features/authentication/data/firebase_auth_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -11,6 +12,8 @@ import 'package:integration_test/integration_test.dart';
 import 'package:patrol_finders/patrol_finders.dart';
 
 void main() {
+  String testEmail = 'bob@example.com';
+  String testPassword = 'foobarbaz';
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
   group('Sample Integration Test', () {
     patrolWidgetTest('Access Profile Screen', (PatrolTester $) async {
@@ -28,7 +31,7 @@ void main() {
       final user = MockUser(
         isAnonymous: false,
         uid: 'someuid',
-        email: 'bob@somedomain.com',
+        email: testEmail,
         displayName: 'Bob',
       );
       FirebaseAuth mockFirebaseAuth = MockFirebaseAuth(mockUser: user);
@@ -46,10 +49,21 @@ void main() {
       User? loggedInUser = mockFirebaseAuth.currentUser;
       expect(loggedInUser, null, reason: 'A user is already signed in');
       // Mock the sign in process.
-      final result = await mockFirebaseAuth.signInWithCredential(credential);
-      expect(result.user?.displayName, 'Bob', reason: 'User not signed in');
+      // final result = await mockFirebaseAuth.signInWithCredential(credential);
+      // await user.updatePassword(testPassword);
+      // expect(result.user?.displayName, 'Bob', reason: 'User not signed in');
       // Verify that we are at the profile screen.
-      expect($(#profileScreen).exists, true, reason: 'Not at Profile screen');
+
+      expect($(EmailInput), findsOneWidget, reason: 'Email input not found');
+      expect($(PasswordInput), findsOneWidget,
+          reason: 'Password input not found');
+      await $(EmailInput)
+          .$(TextFormField)
+          .waitUntilVisible()
+          .enterText(testEmail);
+      await $(PasswordInput).$(TextFormField).enterText(testPassword);
+      await $(ButtonStyleButton).tap();
+      // expect($(#profileScreen).exists, true, reason: 'Not at Profile screen');
     });
   });
 }
